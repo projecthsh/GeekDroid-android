4c32f442c65c1aa1c4ef5f24402fdd3d68ff317b
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2022-10-25 19:59:30 +0800

    Add files via upload

--- mods/fun.lua
+++ mods/fun.lua
@@ -1,1 +1,20 @@
+function 自动安装(路径,文件)
+   io.popen("su -c 'cp -rf "..路径.." /data/local/tmp/"..文件..".apk".."'")
+   io.popen("su -c 'pm install ".."/data/local/tmp/"..文件 ..".apk".."'")
+end
+--自动安装("/storage/emulated/0/base.apk","名称")
 
+function exec(cmd,sh,su)
+  cmd=tostring(cmd)
+  if sh == true then
+    cmd=io.open(cmd):read("*a")
+  end
+  if su == 0 then
+    p=io.popen(string.format('%s',cmd))
+   else
+    p=io.popen(string.format('%s',"su -c "..cmd))
+  end
+  local s=p:read("*a")
+  p:close()
+  return s
+end

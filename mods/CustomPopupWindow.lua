f064013c1c0ee9abea352a6aa57a6aaf8a25c770
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2023-01-03 13:39:46 +0800

    Add files via upload

--- mods/CustomPopupWindow.lua
+++ mods/CustomPopupWindow.lua
@@ -1,20 +1,15 @@
 function showPopMenu(tab,views,title)
-  lp = activity.getWindow().getAttributes();
-  lp.alpha = 0.85;
-  activity.getWindow().setAttributes(lp);
-  activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-
   Popup_layout={
     LinearLayoutCompat;
     {
       MaterialCardView;
-      Elevation="0";
+      id="popContent",
+      Elevation="4dp";
       CardBackgroundColor=onsurfaceInv;
       Radius="18dp";
       layout_width="192dp";
-      layout_height="-2";
-      layout_marginTop="8dp";
-      layout_marginLeft="8dp";
+      layout_height="-2",
+      layout_margin="8dp",
       {
         LinearLayoutCompat;
         layout_height="-1";
@@ -24,9 +19,8 @@ function showPopMenu(tab,views,title)
       };
     };
   };
-
   --PopupWindow
-  pops=PopupWindow(activity)
+  pops=PopupWindow(this)
   --PopupWindow加载布局
   .setContentView(loadlayout(Popup_layout))
   .setWidth(-2)
@@ -36,25 +30,21 @@ function showPopMenu(tab,views,title)
   .setBackgroundDrawable(ColorDrawable(0x00000000))
 
   pops.onDismiss=function()
-    lp = activity.getWindow().getAttributes();
-    lp.alpha = 1;
-    activity.getWindow().setAttributes(lp);
-    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
   end
 
   --PopupWindow列表项布局
   Popup_list_title={
     LinearLayoutCompat;
     layout_width="-1";
-    layout_height="48dp";
+    layout_height="36dp";
     {
       MaterialTextView;
       id="popadp_text";
       Typeface=Typeface.DEFAULT_BOLD,
       textColor=tertiaryc;
+      textSize="14sp";
       layout_width="-1";
       layout_height="-1";
-      textSize="14sp";
       gravity="left|center";
       paddingLeft="16dp";
       Enabled=false,
@@ -68,7 +58,8 @@ function showPopMenu(tab,views,title)
     popadp_text.setText(title)
   end
 
-  Popup_list_item={
+  Popup_list_item=
+  {
     LinearLayoutCompat;
     layout_width="-1";
     layout_height="48dp";
@@ -91,12 +82,11 @@ function showPopMenu(tab,views,title)
     if type(v)=="table"
 
       Popup_list.addView(view)
-      popadp_text.setText(k.."...")
+      popadp_text.setText(k)
       view.onClick=function()
         pops.dismiss()
-        task(50,function()
-
-          showPopMenu(v,views,k.."...")
+        task(30,function()
+          showPopMenu(v,views,k)
         end)
         -- pops.dismiss()
       end
@@ -107,7 +97,7 @@ function showPopMenu(tab,views,title)
       popadp_text.setText(k)
       view.onClick=function()
         pops.dismiss()
-        task(50,function()
+        task(30,function()
 
           v()
         end)
